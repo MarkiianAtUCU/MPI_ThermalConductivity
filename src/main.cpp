adcb28112ae0a4a3114ecff8ce742b02e2c2832f
Markiian Matsiuk <matsiuk@ucu.edu.ua>
2019-10-16 19:44:19 +0300

    Updated read config

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,14 @@
 #include <iostream>
+#include <config_reader.h>
 #include "MpiTable.h"
 
 int main() {
-    std::vector<double> vect{-1, -1,  -1,  -1,  -1, -1,
-                             -1, 10,  20,  30,  40, -1,
-                             -1, 50,  60,  70,  80, -1,
-                             -1, 90, 100, 110, 120, -1,
-                             -1, 130, 140, 150, 160, -1,
-                             -1, 170, 180, 190, 200, -1,
-                             -1, -1,  -1,  -1,  -1, -1};
+    config *c = new config("../config.data");
+    std::vector<double> table(20);
 
-    auto a = MpiTable(4,5, vect);
+    c->get_table(table);
 
-    std::cout << a.get(3, 2) << std::endl;
-    a.set(3, 2, 3.5);
-    a.flip();
-    std::cout << a.get(3, 2) << std::endl;
-    std::cout << a.getTop()[0] << std::endl;
-    std::cout << a.getBottom()[0] << std::endl;
+    for (int i = 0; i < table.size(); ++i) {
+        std::cout << table[i] << std::endl;
+    }
 }
